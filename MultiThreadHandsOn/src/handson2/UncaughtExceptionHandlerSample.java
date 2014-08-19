ad8b98c2cdc26ac7bebd6a6825149283bca217d7
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2014-08-19 21:17:17 +0900

    UncaughtExceptionHandlerのサンプルを追加。

---
+++ MultiThreadHandsOn/src/handson2/UncaughtExceptionHandlerSample.java
@@ -0,0 +1,21 @@
+package handson2;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+public class UncaughtExceptionHandlerSample {
+	public static void main(String[] args) {
+		// UncaughtExceptionHandlerを定義
+		UncaughtExceptionHandler ueh = new UncaughtExceptionHandler() {
+			public void uncaughtException(Thread t, Throwable e) {
+				// 通常、標準エラー出力に出るところ、標準出力に表示するよう変更
+				e.printStackTrace(System.out);
+			}
+		};
+
+		// カレントスレッドにHandlerをセット
+		Thread.setDefaultUncaughtExceptionHandler(ueh);
+		
+		// ランタイム例外を（意図的に）スロー
+		throw new RuntimeException("Runtime Exception!");
+	}
+}
