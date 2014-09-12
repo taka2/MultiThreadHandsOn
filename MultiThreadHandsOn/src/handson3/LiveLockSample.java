eb6699b266ceab6755089c328844665e7e683d93
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2014-09-12 09:01:42 +0900

    ライブロックを再現するサンプルを追加。

---
+++ MultiThreadHandsOn/src/handson3/LiveLockSample.java
@@ -0,0 +1,69 @@
+package handson3;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * ライブロックを再現するサンプル
+ */
+public class LiveLockSample {
+	// スレッドプール数
+	private static final int FIXED_THREAD_POOL_COUNT = 10;
+	
+	public static void main(String[] args) {
+		final Queue<Integer> queue = new ConcurrentLinkedQueue<Integer>();
+		
+		// キューにデータを投入するタスク
+		Runnable enqueueTask = new Runnable() {
+			int counter = 1;
+			public void run() {
+				synchronized(queue) {
+					queue.add(counter);
+				}
+				counter++;
+			}
+		};
+
+		// キューに入ったデータを処理するタスク
+		Runnable dequeueTask = new Runnable() {
+			public void run() {
+				while(true) {
+					synchronized(queue) {
+						if(queue.size() == 0) {
+							continue;
+						}
+
+						// キューからデータの取り出し（削除はしない）
+						Integer data = queue.peek();
+						
+						// データをチェック
+						if(data > 5) {
+							System.out.println("Invalid data. data=" + data);
+	
+							// エラーになったデータはキューから削除しないでリトライ
+							continue;
+						}
+						
+						// データを処理
+						System.out.println("Process data. data=" + data);
+						
+						// キューからデータ取り出し＆削除
+						queue.poll();
+					}
+				}
+			}
+		};
+		
+		// キューにデータを投入するタスクは1秒間隔で実行
+		ScheduledExecutorService scheduledService = Executors.newScheduledThreadPool(1);
+		scheduledService.scheduleAtFixedRate(enqueueTask, 0, 1, TimeUnit.SECONDS);
+		
+		// キューに入ったデータを処理するタスクを実行
+		ExecutorService service = Executors.newFixedThreadPool(FIXED_THREAD_POOL_COUNT);
+		service.submit(dequeueTask);
+	}
+}
