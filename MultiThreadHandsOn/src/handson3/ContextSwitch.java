3d7bce797d125066034901b82568063cd675c093
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2014-09-16 21:36:03 +0900

    コンテキストスイッチのコストを見るためのサンプルを追加。

---
+++ MultiThreadHandsOn/src/handson3/ContextSwitch.java
@@ -0,0 +1,38 @@
+package handson3;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * コンテキストスイッチのコストを見るためのサンプル（シングルスレッド版）
+ */
+public class ContextSwitch {
+	// 計算量
+	private static final int CALC_AMOUNT = 1000000;
+
+	public static void main(String[] args) {
+		// 何か計算するタスク
+		Runnable task = new Runnable() {
+			public void run() {
+				for(int i=0; i<CALC_AMOUNT; i++) {
+					Math.pow(i, 2);
+				}
+			}
+		};
+		
+		long startTime = System.currentTimeMillis();
+		ExecutorService service = Executors.newSingleThreadExecutor();
+		for(int i=0; i<100; i++) {
+			service.submit(task);
+		}
+		service.shutdown();
+		try {
+			service.awaitTermination(5, TimeUnit.SECONDS);
+		} catch(InterruptedException e) {
+			// 何もしない
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("time = " + (endTime - startTime) + "[ms]");
+	}
+}
---
+++ MultiThreadHandsOn/src/handson3/ContextSwitch2.java
@@ -0,0 +1,38 @@
+package handson3;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * コンテキストスイッチのコストを見るためのサンプル（マルチスレッド版）
+ */
+public class ContextSwitch2 {
+	// 計算量
+	private static final int CALC_AMOUNT = 1000000;
+
+	public static void main(String[] args) {
+		// 何か計算するタスク
+		Runnable task = new Runnable() {
+			public void run() {
+				for(int i=0; i<CALC_AMOUNT; i++) {
+					Math.pow(i, 2);
+				}
+			}
+		};
+
+		long startTime = System.currentTimeMillis();
+		ExecutorService service = Executors.newFixedThreadPool(100);		
+		for(int i=0; i<100; i++) {
+			service.submit(task);
+		}
+		service.shutdown();
+		try {
+			service.awaitTermination(60, TimeUnit.SECONDS);
+		} catch(InterruptedException e) {
+			// 何もしない
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("time = " + (endTime - startTime) + "[ms]");
+	}
+}
